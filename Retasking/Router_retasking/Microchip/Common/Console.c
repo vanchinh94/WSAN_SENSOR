d3ad46c82f9fdc64d14e3520fdbfd7389eee47b2
vanchinh94 <chinhbka.k57@gmail.com>
2018-04-03 13:36:06 +0700

    delete space

--- Retasking/Router_retasking/Microchip/Common/Console.c
+++ Retasking/Router_retasking/Microchip/Common/Console.c
@@ -59,8 +59,7 @@
 // Or you may also globally enable debug by defining this macro
 // in zigbee.def file or from compiler command-line.
 #ifndef ENABLE_DEBUG
-//#define ENABLE_DEBUG
-#endif
+#define ENABLE_DEBUG
 
 #include "Console.h"
 #include "zigbee.h"
@@ -91,7 +90,6 @@
     void ConsolePutROMString(ROM char* str)
     {
         BYTE c;
-
         while( (c = *str++) != 0 )
         ConsolePut(c);
     }
@@ -128,88 +126,78 @@
     BYTE ConsoleGet(void)
     {
         char Temp;
-
-//        while(IFS1bits.U2RXIF == 0);
+        while(IFS1bits.U2RXIF == 0);
         while(U2STAbits.URXDA == 0);
         Temp = U2RXREG;
-//        IFS1bits.U2RXIF = 0;
+        IFS1bits.U2RXIF = 0;
         return Temp;
     }
 
-    /* Writen by Hung Nguyen */
-    //#ifdef USE_MINI_RADAR
+    /*UART1 */
         void UART1Init(void)
-        {
-            U1BRG  = (CLOCK_FREQ/2/4)/BAUD_RATE_UART1-1;
+    {
+        U1BRG  = (CLOCK_FREQ/2/4)/BAUD_RATE_UART1-1;
+        U1MODE = 0x8008;//enable UART1
+                        //U1TX and U1RX pins are enabled and used
+                        //Standard Speed mode (16 BRG clock cycles per bit)
+                        //8-bit data, no parity
+                        //One Stop bit
+        U1STA = 0x0400;//Transmit is enabled, U1TX pin is controlled by UART1
+        IEC0bits.U1RXIE = 1;
+        IFS0bits.U1RXIF = 0;//enable UART1 interrupt
+    }
 
-            IEC0bits.U1RXIE = 1;
-            IFS0bits.U1RXIF = 0;//enable UART1 interrupt
+    void UART1Put(BYTE c)
+    {
+        while(U1STAbits.TRMT == 0);
+        U1TXREG = c;
+    }
 
-            U1MODE = 0x8008;//enable UART1
-                            //U1TX and U1RX pins are enabled and used
-                            //Standard Speed mode (16 BRG clock cycles per bit)
-                            //8-bit data, no parity
-                            //One Stop bit
-            U1STA = 0x0400;//Transmit is enabled, U1TX pin is controlled by UART1
-        }
-
-        void UART1Put(BYTE c)
-        {
-            while(U1STAbits.TRMT == 0);
-            U1TXREG = c;
-        }
-
-        void UART1PutROMString(ROM char* str)
-        {
-            BYTE c;
-
-            while( (c = *str++) != 0 )
-            UART1Put(c);
-        }
+    void UART1PutROMString(ROM char* str)
+    {
+        BYTE c;
+        while( (c = *str++) != 0 )
+        UART1Put(c);
+    }
 
 
 //	    ROM unsigned char CharacterArray[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        void UART1PrintChar(BYTE toPrint)
-        {
-            BYTE PRINT_VAR;
-            PRINT_VAR = toPrint;
-            toPrint = (toPrint>>4)&0x0F;
-            UART1Put(CharacterArray[toPrint]);
-            toPrint = (PRINT_VAR)&0x0F;
-            UART1Put(CharacterArray[toPrint]);
-            return;
-        }
+    void UART1PrintChar(BYTE toPrint)
+    {
+        BYTE PRINT_VAR;
+        PRINT_VAR = toPrint;
+        toPrint = (toPrint>>4)&0x0F;
+        UART1Put(CharacterArray[toPrint]);
+        toPrint = (PRINT_VAR)&0x0F;
+        UART1Put(CharacterArray[toPrint]);
+        return;
+    }
 
-        void UART1PrintWord(WORD toPrint)
-        {
-            BYTE MSB,LSB;
-            MSB = toPrint >> 8;
-            LSB = toPrint & 0x00FF;
-            UART1PrintChar(MSB);
-            UART1PrintChar(LSB);
-        }
+    void UART1PrintWord(WORD toPrint)
+    {
+        BYTE MSB,LSB;
+        MSB = toPrint >> 8;
+        LSB = toPrint & 0x00FF;
+        UART1PrintChar(MSB);
+        UART1PrintChar(LSB);
+    }
 
-        BYTE UART1ConsoleGet(void)
-        {
-            char Temp;
+    BYTE UART1ConsoleGet(void)
+    {
+        char Temp;
 
-            while(IFS0bits.U1RXIF == 0);
+        while(IFS0bits.U1RXIF == 0);
 
-            Temp = U1RXREG;
-            IFS0bits.U1RXIF = 0;
-            return Temp;
-        }
-   // #endif
-
-    /* End by Hung Nguyen*/
-
+        Temp = U1RXREG;
+        IFS0bits.U1RXIF = 0;
+        return Temp;
+    }
 #elif defined(__18CXX)
 // This is only until they fix the include file...
 #ifdef __18F87J10
     #define RCREG   RCREG1
     #define BAUDCON BAUDCON1
 #endif
-
 
 #define USART_USE_BRGH_HIGH
 
@@ -219,15 +207,12 @@
    	#define SPBRG_VAL   ( ((CLOCK_FREQ/BAUD_RATE)/16) - 1)
 #endif
 
-
-
 #if SPBRG_VAL > 255
     #error "Calculated SPBRG value is out of range for currnet CLOCK_FREQ."
 #endif
 
 void ConsoleInit(void)
 {
-
     RCSTAbits.SPEN = 1;   // (RCSTA<7>) must be set (= 1),
     TRISCbits.TRISC6 = 0;
     TRISCbits.TRISC7 = 1;
@@ -246,7 +231,6 @@ void ConsoleInit(void)
 void ConsolePutROMString(ROM char* str)
 {
     BYTE c;
-
     while( c = *str++ )
         ConsolePut(c);
 
@@ -255,7 +239,6 @@ void ConsolePutROMString(ROM char* str)
     // statement after the function call.
     while( !ConsoleIsPutReady() );
 }
-
 
 BYTE ConsoleGetString(char *buffer, BYTE bufferLen)
 {
@@ -308,7 +291,6 @@ BYTE ConsoleGet(void)
         RCSTAbits.CREN = 0;   // Disable UART receiver
         RCSTAbits.CREN = 1;   // Enable UART receiver
     }
-
     return RCREG;
 }
 
